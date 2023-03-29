58691ccca1541446b1ef8d7716dba303c83ffbd8
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2023-03-29 16:59:39 +0200

    Add assembler

---
+++ src/sas.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python3
+
+from abc import ABC
+
+
+class Operand(ABC):
+    def bit_letter(self):
+        return self._bit_letter
+
+
+class Instruction:
+    def __init__(self, operands: list[Operand], instruction_encoding):
+        self._operands = operands
+        self._instruction_encoding = instruction_encoding
+
+        if len(instruction_encoding) != 16:
+            raise ValueError(f'Expected instruction encoding of length 16, got "{instruction_encoding}"')
+
+        bit_letters = set()
+        for operand in self._operands:
+            bit_letter = operand.bit_letter()
+            if not bit_letter:
+                continue
+
+            if bit_letter in bit_letters:
+                raise ValueError(f'Multiple operands with same bit letter {operand.bit_letter()}')
+
+            bit_letters.add(operand.bit_letter())
+
+    def match(self, input: list[str]) -> dict[str, int]:
+        if len(input) != len(self._operands):
+            return None
+
+        result = {}
+        for i, operand in enumerate(self._operands):
+            match_result = operand.match(input[i])
+            if match_result is None:
+                return None
+
+            bit_letter = operand.bit_letter()
+            if bit_letter:
+                assert bit_letter not in result
+                result[bit_letter] = match_result
+
+        return result
+
+    def assemble(self, operand_map: dict[str, int]):
+        result = 0
+
+        counters = {}
+        for bit_letter in operand_map:
+            counters[bit_letter] = 0
+
+        for i, bit_letter in enumerate(reversed(self._instruction_encoding)):
+            if bit_letter == '0':
+                bit = 0
+            elif bit_letter == '1':
+                bit = 1
+            elif bit_letter in [ chr(c) for c in range(ord('a'), ord('z')) ]:
+                bit_position = counters[bit_letter]
+                bit_mask = 1 << bit_position
+                bit = (operand_map[bit_letter] & bit_mask) >> bit_position
+                counters[bit_letter] += 1
+            else:
+                assert False
+
+            result = result | (bit << i)
+        
+        return result
+
+
+class Mnemonic(Operand):
+    def __init__(self, mnemonic: list[str]):
+        self._mnemonic = mnemonic
+        self._bit_letter = None
+
+    def match(self, input: str):
+        if input in self._mnemonic:
+            return []
+        return None
+
+
+class Mnemonics(Operand):
+    def __init__(self, mnemonics: list[list[str]], bit_letter: str):
+        self._mnemonics = mnemonics
+        if len(bit_letter) != 1:
+            raise ValueError(f'Expected bit letter of length 1, got "{bit_letter}"')
+
+        self._bit_letter = bit_letter
+
+    def match(self, input: str):
+        for index, aliases in enumerate(self._mnemonics):
+            if input in aliases:
+                return index
+        return None
+
+
+class RegisterOperand(Operand):
+    def __init__(self, bit_letter: str):
+        if len(bit_letter) != 1:
+            raise ValueError(f'Expected bit letter of length 1, got "{bit_letter}"')
+
+        self._bit_letter = bit_letter
+
+    def match(self, input: str):
+        if len(input) < 2:
+            return None
+
+        if input[0] != 'r':
+            return None
+
+        try:
+            reg_number = [ f'r{i}' for i in range(0, 16) ].index(input)
+        except ValueError:
+            return None
+
+        return reg_number
+
+
+def parse_decimal(input: str):
+    try:
+        return int(input)
+    except ValueError:
+        return None
+
+
+def parse_hexadecimal(input: str):
+    try:
+        return int(input, 16)
+    except ValueError:
+        return None
+
+
+class UImm8(Operand):
+    def __init__(self, bit_letter: str):
+        if len(bit_letter) != 1:
+            raise ValueError(f'Expected bit letter of length 1, got "{bit_letter}"')
+
+        self._bit_letter = bit_letter
+
+    def match(self, input: str):
+        n = parse_decimal(input) or parse_hexadecimal(input)
+        if n is None:
+            return None
+
+        if 0 <= n and n <= 255:
+            return n
+
+        return None
+
+
+class SImm8(Operand):
+    def __init__(self, bit_letter: str):
+        if len(bit_letter) != 1:
+            raise ValueError(f'Expected bit letter of length 1, got "{bit_letter}"')
+
+        self._bit_letter = bit_letter
+
+    def match(self, input: str):
+        n = parse_decimal(input) or parse_hexadecimal(input)
+        if n is None:
+            return None
+
+        if -128 <= n and n <= 127:
+            return n
+
+        return None
+
+
+class SImm4(Operand):
+    def __init__(self, bit_letter: str):
+        if len(bit_letter) != 1:
+            raise ValueError(f'Expected bit letter of length 1, got "{bit_letter}"')
+
+        self._bit_letter = bit_letter
+
+    def match(self, input: str):
+        n = parse_decimal(input) or parse_hexadecimal(input)
+        if n is None:
+            return None
+
+        if -8 <= n and n <= 7:
+            return n
+
+        return None
+
+
+class Assembler:
+    def __init__(self, instructions: list[Instruction]):
+        self._instructions = instructions
+
+    def _match(self, input: list[str]) -> tuple[Operand, ]:
+        for instruction in self._instructions:
+            match = instruction.match(input)
+            if match:
+                return instruction, match
+        
+        return None
+
+    def _parse(self, input: str) -> list[str]:
+        operand, rest = input.split(' ', 1)
+        return [operand] + [x.strip() for x in rest.split(',')]
+
+    def assemble(self, input: str) -> int:
+        match = self._match(self._parse(input))
+        if match is None:
+            raise ValueError(f'No matching instruction found for "{input}"')
+
+        instruction, operand_map = match
+        encoding = instruction.assemble(operand_map)
+        return encoding
+
+
+def generate_conditions(base: str):
+    b = base
+    return [
+        [f'{b}z', f'{b}eq'],
+        [f'{b}nz', f'{b}ne'],
+        [f'{b}a', f'{b}nbe'],
+        [f'{b}ae', f'{b}nb', f'{b}nc'],
+        [f'{b}b', f'{b}nae', f'{b}c'],
+        [f'{b}be', f'{b}na'],
+        [f'{b}g', f'{b}nle'],
+        [f'{b}ge', f'{b}nl'],
+        [f'{b}l', f'{b}nge'],
+        [f'{b}le', f'{b}ng'],
+        [f'{b}n'],
+        [f'{b}p'],
+        [f'{b}nn'],
+        [f'{b}np']
+    ]
+
+
+def printVHDL(input):
+    while len(input) < 32:
+        input.append(0)
+
+    print('"' + '", "'.join(map(lambda x: bin(x)[2:].rjust(16, '0'), input)) + '"')
+
+
+assembler = Assembler([
+    Instruction([Mnemonic(['nop'])], '0000000000000000'),
+    Instruction([Mnemonic(['branch']), RegisterOperand('a')], '00000010aaaa0000'),
+    Instruction([Mnemonics(generate_conditions('branch_'), 'c'), RegisterOperand('a')], '00000001aaaacccc'),
+    Instruction([Mnemonics([['setbyte0'], ['setbyte1'], ['setbyte2'], ['setbyte3']], 'n'), RegisterOperand('a'), UImm8('i')], '01nniiiiaaaaiiii'),
+    Instruction([Mnemonics([['copy'], ['add'], ['subtract'], ['multiply'], ['and'], ['or'], ['xor'], ['not'], ['cmp'], ['test']], 'n'), RegisterOperand('a'), RegisterOperand('b')], '0001nnnnaaaabbbb'),
+    Instruction([Mnemonics([['loadbyte'], ['loadhalfword'], ['loadword']], 's'), RegisterOperand('a'), RegisterOperand('b')], '000010ssaaaabbbb'),
+    Instruction([Mnemonics(['storebyte', 'storehalfword', 'storeword'], 's'), RegisterOperand('a'), RegisterOperand('b')], '000011ssaaaabbbb'),
+    Instruction([Mnemonic(['setsigned']), RegisterOperand('a'), SImm8('i')], '0010iiiiaaaaiiii'),
+    Instruction([Mnemonic(['setunsigned']), RegisterOperand('a'), UImm8('i')], '0011iiiiaaaaiiii'),
+    Instruction([Mnemonic(['prefetch']), RegisterOperand('a')], '10000000aaaa0000'),
+    Instruction([Mnemonic(['flush']), RegisterOperand('a')], '10000000aaaa0001'),
+    Instruction([Mnemonics(generate_conditions('set_'), 'c'), RegisterOperand('a'), RegisterOperand('b')], '1001ccccaaaabbbb'),
+    Instruction([Mnemonics(generate_conditions('set_'), 'c'), RegisterOperand('a'), SImm4('b')], '1010ccccaaaabbbb'),
+])
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) != 2:
+        print('expected a single input file as argument')
+        exit(1)
+
+
+    result = []
+
+    for line in open(sys.argv[1], 'r'):
+        stripped_line = line.strip()
+        if stripped_line == '' or stripped_line[0] == ';':
+            continue
+
+        result.append(assembler.assemble(line))
+
+    printVHDL(result)
