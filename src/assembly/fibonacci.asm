fc8bac69e3d523f2d926d46811ba44c58efbb4f6
Ruben van Nieuwpoort <ruben@vannieuwpoort.dev>
2023-03-31 19:27:24 +0200

    Optimize Fibonacci program

--- src/assembly/fibonacci.asm
+++ src/assembly/fibonacci.asm
@@ -1,15 +1,21 @@
 ; computes the 42th Fibonacci number in r0
-; simulate until values in registers are stable, should run in ~7us
-setbyte0 r1, 1
-setbyte0 r3, 42
+; simulate until values in registers are stable, should run in ~4.5us
+setunsigned r1, 1
+setunsigned r3, 42
 setunsigned r4, 1
-setunsigned r5, 5
-setunsigned r6, 12
+setunsigned r5, 12
+setunsigned r6, 8
+setunsigned r7, 13
 test r3, r3
-branch_z r6
-copy r2, r1
-add r1, r0
-copy r0, r2
-subtract r3, r4
-branch r5
-branch r6
+branch r5  ; branch test
+; begin_loop:
+    copy r2, r1
+    add r1, r0
+    copy r0, r2
+    subtract r3, r4
+
+; test:
+branch_nz r6  ; branch_nz begin_loop
+
+; end:
+branch r7  ; branch end
